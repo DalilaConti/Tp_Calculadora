d41868ee0b0d687b994fb8b60b9ce8ec75abbd36
DalilaConti <dalila.contigoyeneche@gmail.com>
2022-03-23 09:04:44 -0300

    Subiendo archivos js calculadora

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+let sumarRequerida= require ('./sumar');
+let restarRequerida= require ('./restar');
+let dividirRequerida= require ('./dividir');
+let multiplicarRequerida = require ('./multiplicar');
+
+console.log(sumarRequerida(10,20))
+console.log(restarRequerida(5,1))
+console.log(dividirRequerida(0,8))
+console.log(multiplicarRequerida(0,2))
+
---
+++ dividir.js
@@ -0,0 +1,8 @@
+function dividir(a,b){
+    if (a=== 0){
+    return "no se puede dividir por 0"
+    }
+return a/b;
+}
+module.exports = dividir;
+
